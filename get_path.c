967777d4f02d61d9e2b42a8a144eb98e99844cbf
marwenrebah <6863@holbertonstudents.com>
2023-08-25 10:15:54 -1200

    The executable path

--- get_path.c
+++ get_path.c
@@ -1,27 +1,47 @@
 #include "shell.h"
 /**
- * get_path - retrieves the executable path for a command.
- * @cmd: the command line.
+ * check_path - Check if the command exists in the system's PATH directories.
+ * @command: The command to check.
+ * Return: The full path of the command if found, or NULL if not found.
  */
-void get_path(char **cmd)
+char *check_path(char *command)
 {
-int i;
-char **tok = NULL, *path = strdup(getenv("PATAH")), *tmp = NULL;
-tok = str_split(path, ":");
-free(path);
-path = NULL;
-for (i = 0; tok[i]; i++)
+char *array[1000], *path = strdup(getenv("PATH"));
+char *path_token = strtok(path, ":");
+int i = 0;
+char *final_path;
+struct stat info;
+/*  If the command is found in the current directory. */
+if (stat(command, &info) == 0)
 {
-tmp = calloc(sizeof(char), (strlen(tok[i]) + 1 + strlen(cmd[0]) + 1));
-strcat(tmp, tok[i]);
-strcat(tmp, "/");
-strcat(tmp, cmd[0]);
-if (access(tmp, F_OK) == 0)
-break;
-free(tmp);
-tmp = NULL;
+strcpy(path, command);
+return (path);
 }
-free_array(tok);
-free(cmd[0]);
-cmd[0] = tmp;
+final_path = (char *)malloc(1024 * sizeof(char));
+/*Loop through each directory in the PATH*/
+while (path_token != NULL)
+{
+array[i] = path_token;
+i++;
+path_token = strtok(NULL, ":");
+}
+i = 0;
+/* Search for the command in each directory. */
+while (array[i] != NULL)
+{
+strcpy(final_path, array[i]);
+strcat(final_path, "/");
+strcat(final_path, command);
+strcat(final_path, "\0");
+/* If the command is found in the directory. */
+if (stat(final_path, &info) == 0)
+{
+free(path);
+return (final_path);
+}
+i++;
+}
+free(final_path);
+free(path);
+return (NULL);
 }
