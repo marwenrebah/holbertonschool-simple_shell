3224766daa0db2b7bbe9d65ccd6bb7cc92edd193
marwenrebah <6863@holbertonstudents.com>
2023-08-25 08:45:34 -1200

    Execute a command

---
+++ execute.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+/**
+ * exec_command - Execute a command.
+ * @cmd: The command to be executed.
+ */
+void exec_command(char **cmd)
+{
+pid_t pid = 0;
+int status = 0;
+if (!cmd)
+return;
+pid = fork();
+if (pid == -1)
+{
+perror("Error in Fork");
+}
+else if (pid > 0)
+{
+waitpid(pid, &status, 0);
+}
+else
+{
+if (execvp(cmd[0], cmd) == -1)
+{
+perror(cmd[0]);
+exit(EXIT_FAILURE);
+}
+}
+}
+/**
+ * sigintHandler - Handle the Control-C signal.
+ * @sig_num: The signal number.
+ */
+void sigintHandler(__attribute__((unused)) int sig_num)
+{
+signal(SIGINT, sigintHandler);
+printf("\n");
+write(STDOUT_FILENO, "$ ", 2);
+}
+
