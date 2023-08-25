6392ad670549aaec4d02ebed4b2c73a3597a7cf9
marwenrebah <6863@holbertonstudents.com>
2023-08-25 11:00:35 -1200

    Prints the environment variables

--- print_env.c
+++ print_env.c
@@ -1,14 +1,28 @@
 #include "shell.h"
 /**
- * _env - Prints the environment variables.
+ * print_environment - Print the environment variables.
  */
-void _env(void)
+void print_environment(void)
 {
-int i = 0;
-while (environ[i] != NULL)
+char **s = environ;
+for (; *s; s++)
 {
-printf("%s\n", environ[i]);
-i++;
+printf("%s\n", *s);
 }
 }
-
+/**
+ * if_conditions - Check for built-in commands.
+ * @cmd: The command.
+ * @line: The input line.
+ */
+void if_conditions(char **cmd, char *line)
+{
+if (strcmp(cmd[0], "exit") == 0)
+{
+free(line);
+if (cmd[1])
+exit(atoi(cmd[1]));
+free_array(cmd);
+exit(0);
+}
+}
