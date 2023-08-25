22d9e2e1e3ea24d68f4829b6dee3a057d76077da
marwenrebah <6863@holbertonstudents.com>
2023-08-25 10:59:17 -1200

    Execute a command

--- execute.c
+++ execute.c
@@ -1,27 +1,39 @@
 #include "shell.h"
 /**
- * execute_line - Execute a command using the execve function.
- * @tokens: The array of command and arguments to execute.
- * @path: The full path to the executable command.
- * Return: The exit status of the executed command.
+ * exec_command - Execute a command.
+ * @cmd: The command to be executed.
  */
-int execute_line(char **tokens, char *path)
+void exec_command(char **cmd)
 {
+pid_t pid = 0;
 int status = 0;
-pid_t pid;
+if (!cmd)
+return;
 pid = fork();
-if (pid == 0)
+if (pid == -1)
 {
-/*Execute the command using the specified path and arguments*/
-execve(path, tokens, environ);
-perror("Execution failed");
-exit(EXIT_FAILURE);
+perror("Error in Fork");
+}
+else if (pid > 0)
+{
+waitpid(pid, &status, 0);
 }
 else
 {
-wait(&status);
-if (WIFEXITED(status))
-status = WEXITSTATUS(status);
+if (execvp(cmd[0], cmd) == -1)
+{
+perror(cmd[0]); /*Print an error message*/
+exit(EXIT_FAILURE);
 }
-return (status);
+}
+}
+/**
+ * sigintHandler - Handle the Control-C signal.
+ * @sig_num: The signal number.
+ */
+void sigintHandler(__attribute__((unused)) int sig_num)
+{
+signal(SIGINT, sigintHandler);
+printf("\n");
+write(STDOUT_FILENO, "$ ", 2);
 }
