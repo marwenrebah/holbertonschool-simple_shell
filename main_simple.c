757d51861ebaad5b0de496794c7b0332baaa0f70
marwenrebah <6863@holbertonstudents.com>
2023-08-25 10:16:18 -1200

    Main function for simple shell

--- main_simple.c
+++ main_simple.c
@@ -1,72 +1,44 @@
 #include "shell.h"
 /**
- * free_array - Frees a dynamically allocated array of strings.
- * @array: The array to be freed.
- */
-void free_array(char **array)
-{
-int i;
-for (i = 0; array[i]; i++)
-{
-free(array[i]);
-array[i] = NULL;
-}
-free(array);
-array = NULL;
-}
-/**
- * loop - Main loop of the shell.
- * @input: 1 if shell is interactive, 0 if batch mode.
- */
-void loop(int input)
-{
-char *line = NULL, **cmd = NULL;
-size_t size = 0;
-int length;
-struct stat state;
-while (1)
-{
-if (input)
-write(STDOUT_FILENO, "$ ", 2);
-length = getline(&line, &size, stdin);
-if (length == EOF)
-{
-free(line);
-exit(0);
-}
-if (strcmp(line, "\n") == 0)
-continue;
-cmd = str_split(line, " \t\r\n");
-if_conditions(cmd, line);
-if (strcmp(cmd[0], "env") == 0)
-{
-print_environment();
-free_array(cmd);
-continue;
-}
-if (stat(cmd[0], &state) != 0)
-get_path(cmd);
-if (cmd[0] == NULL)
-{
-printf("Command not found\n");
-free(line);
-line = NULL;
-free_array(cmd);
-cmd = NULL;
-continue;
-}
-exec_command(cmd);
-free_array(cmd);
-}
-free(line);
-}
-/**
- * main - Entry point of the shell program.
+ * main - Entry point for the simple shell.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
  * Return: Always returns 0.
  */
-int main(void)
+int main(int argc, char **argv)
 {
-signal(SIGINT, sigintHandler);
-loop(isatty(STDIN_FILENO));
+char *readline, **tokens, *path = NULL;
+int status;
+(void) argc;
+(void) argv;
+while (1)
+{
+if (isatty(STDIN_FILENO))
+printf("$ ");
+readline = read_line();
+/*Check if the user entered the "env" command*/
+if (strcmp("env\n", readline) == 0)
+{
+free(readline);
+_env();
+continue;
+}
+tokens = parse_the_line(readline, TOK_DELIM);
+if (*tokens == NULL)
+{
+free_d_p(tokens);
+continue;
+}
+/*Check if the command exists in the PATH*/
+path = check_path(*tokens);
+if (path != NULL)
+{
+/*Execute the command and get its exit status*/
+status = execute_line(tokens, path);
+free(path);
+}
+free_d_p(tokens);
+}
+status = 1;
 return (0);
 }
