87520026d9232ee2370d8849050abc41cad6e64f
marwenrebah <6863@holbertonstudents.com>
2023-08-31 20:48:41 -1200

    Header

---
+++ shell.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define TOK_DELIM " \t\n" /*Delimiter used for tokenizing input*/ 
+#define SIZE 64
+
+extern char **environ;
+
+char *read_line(void);
+char **parse_the_line(char *, const char *);
+int execute_line(char **, char *);
+void _env(void);
+char *check_path(char *);
+void free_d_p(char **);
+
+#endif
