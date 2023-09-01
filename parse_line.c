f1459276556eaf2eeba45319f4234a2abc0b8a66
marwenrebah <6863@holbertonstudents.com>
2023-08-31 22:45:46 -1200

    Parses the input line into an array of words

--- parse_line.c
+++ parse_line.c
@@ -9,23 +9,23 @@
  */
 char **parse_the_line(char *readline, const char *separator)
 {
-    int i = 0, size = SIZE;
-    char **tokens = (char **) malloc(size * sizeof(char *)), *token = NULL;
+	int i = 0, size = SIZE;
+	char **tokens = (char **) malloc(size * sizeof(char *)), *token = NULL;
 
-    /*Tokenize the input string using the specified separator*/ 
-    token = strtok(readline, separator);
+	/*Tokenize the input string using the specified separator*/
+	token = strtok(readline, separator);
 
-    /*Iterate through the tokens and store them in the tokens array*/ 
-    while (token != NULL)
-    {
-        tokens[i] = strdup(token);
-        i++;
-        token = strtok(NULL, separator);
-    }
+	/*Iterate through the tokens and store them in the tokens array*/
+	while (token != NULL)
+	{
+		tokens[i] = strdup(token);
+		i++;
+		token = strtok(NULL, separator);
+	}
 
-    tokens[i] = NULL;
+	tokens[i] = NULL;
 
-    free(readline);
+	free(readline);
 
-    return (tokens);
+	return (tokens);
 }
