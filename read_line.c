73a282b834bb54a11e209f3f046c30e22148dedd
marwenrebah <6863@holbertonstudents.com>
2023-08-31 22:46:06 -1200

    Reads a line of text from standard input

--- read_line.c
+++ read_line.c
@@ -7,24 +7,24 @@
  */
 char *read_line(void)
 {
-    char *input = NULL;
-    size_t bufsize = 0;
-    ssize_t input_len;
+	char *input = NULL;
+	size_t bufsize = 0;
+	ssize_t input_len;
 
-    /*Read a line from standard input into the 'input' buffer*/ 
-    input_len = getline(&input, &bufsize, stdin);
-    if (input_len == -1)
-    {
-        free(input);
-        /*Check if the end-of-file (EOF) was reached*/ 
-        if (feof(stdin))
-            exit(0);
-        else
-        {
-            perror("getline failed");
-            exit(1);
-        }
-    }
+	/*Read a line from standard input into the 'input' buffer*/
+	input_len = getline(&input, &bufsize, stdin);
+	if (input_len == -1)
+	{
+		free(input);
+		/*Check if the end-of-file (EOF) was reached*/
+		if (feof(stdin))
+			exit(0);
+		else
+		{
+			perror("getline failed");
+			exit(1);
+		}
+	}
 
-    return (input);
+	return (input);
 }
