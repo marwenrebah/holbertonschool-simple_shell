45887ccbfa0d284a25e954bac405972b922d44e7
marwenrebah <6863@holbertonstudents.com>
2023-08-31 22:43:21 -1200

    Executes a command

--- execute_line.c
+++ execute_line.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * exec_command - Executes a command by forking a new process and using execve.
+ * execute_line - Executes a command by forking a new process and using execve.
  * @tokens: The vector containing the command and its arguments.
  * @path: The full path to the command executable.
  *
@@ -9,31 +9,32 @@
  */
 int execute_line(char **tokens, char *path)
 {
-    int status = 0;
-    pid_t pid;
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Fork failed");
-        exit(1);
-    }
+	int status = 0;
+	pid_t pid;
 
-    if (pid == 0)
-    {
-        /*Child process: execute the command*/ 
-        execve(path, tokens, environ);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		exit(1);
+	}
 
-        /*If execve fails, print an error message and exit*/ 
-        perror("execve failed");
-        exit(2);
-    }
-    else
-    {
-        /*Parent process: wait for the child process to finish*/ 
-        wait(&status);
-        if (WIFEXITED(status))
-            status = WEXITSTATUS(status);
-    }
+	if (pid == 0)
+	{
+		/*Child process: execute the command*/
+		execve(path, tokens, environ);
 
-    return (status);
+		/*If execve fails, print an error message and exit*/
+		perror("execve failed");
+		exit(2);
+	}
+	else
+	{
+		/*Parent process: wait for the child process to finish*/
+		wait(&status);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status);
+	}
+
+	return (status);
 }
