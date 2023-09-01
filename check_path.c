d5b31c4857172cf47417106e525b8c33a79cca68
marwenrebah <6863@holbertonstudents.com>
2023-08-31 22:43:01 -1200

    Checks if a command exists in the system's PATH

--- check_path.c
+++ check_path.c
@@ -8,48 +8,44 @@
  */
 char *check_path(char *command)
 {
-    char *array[1000], *path = strdup(getenv("PATH"));
-    char *path_token = strtok(path, ":");
-    int i = 0;
-    char *final_path;
-    struct stat info;
+	char *array[1000], *path = strdup(getenv("PATH"));
+	char *path_token = strtok(path, ":");
+	int i = 0;
+	char *final_path;
+	struct stat info;
 
-    /*Check if the command exists in the current directory*/ 
-    if (stat(command, &info) == 0)
-    {
-        strcpy(path, command);
-        return (path);
-    }
+	/*Check if the command exists in the current directory*/
+	if (stat(command, &info) == 0)
+	{
+		strcpy(path, command);
+		return (path);
+	}
 
-    final_path = (char *) malloc(1024 * sizeof(char));
+	final_path = (char *) malloc(1024 * sizeof(char));
 
-    /* Tokenize the PATH variable into an array of directories*/
-    while (path_token != NULL)
-    {
-        array[i] = path_token;
-        i++;
-        path_token = strtok(NULL, ":");
-    }
-    i = 0;
-    while (array[i] != NULL)
-    {
-        strcpy(final_path, array[i]);
-        strcat(final_path, "/");
-        strcat(final_path, command);
-        strcat(final_path, "\0");
-
-        /*Check if the command exists in this directory*/ 
-        if (stat(final_path, &info) == 0)
-        {
-            free(path);
-            return (final_path);
-        }
-        i++;
-    }
-
-    free(final_path);
-    free(path);
-
-    /*If the command is not found in any directory in the PATH, return NULL*/ 
-    return (NULL);
+	/* Tokenize the PATH variable into an array of directories*/
+	while (path_token != NULL)
+	{
+		array[i] = path_token;
+		i++;
+		path_token = strtok(NULL, ":");
+	}
+	i = 0;
+	while (array[i] != NULL)
+	{
+		strcpy(final_path, array[i]);
+		strcat(final_path, "/");
+		strcat(final_path, command);
+		strcat(final_path, "\0");
+		/*Check if the command exists in this directory*/
+		if (stat(final_path, &info) == 0)
+		{
+			free(path);
+			return (final_path);
+		}
+		i++;
+	}
+	free(final_path);
+	free(path);
+	return (NULL);
 }
