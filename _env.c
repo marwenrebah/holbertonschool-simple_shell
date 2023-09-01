57d1a8386ea49cfc21d36eb5e0152ca63fe8bd0d
marwenrebah <6863@holbertonstudents.com>
2023-08-31 22:42:39 -1200

    Prints the environment variables

--- _env.c
+++ _env.c
@@ -5,11 +5,12 @@
  */
 void _env(void)
 {
-    char *s = environ[0];
-    int i = 0;
-    while (s != NULL)
-    {
-        printf("%s\n", environ[i]); /*Print the current environment variable*/ 
-        s = environ[++i]; /*Move to the next environment variable*/ 
-    }
+	char *s = environ[0];
+	int i = 0;
+
+	while (s != NULL)
+	{
+		printf("%s\n", environ[i]); /*Print the current environment variable*/
+		s = environ[++i]; /*Move to the next environment variable*/
+	}
 }
